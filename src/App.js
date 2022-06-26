4d9d31fd957205832cab9adba566da5e26e2966c
Deena Smith <deenasmith1630@gmail.com>
2022-06-26 11:58:29 -0500

    nav bar with styling added, also mobile responsive

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import React from 'react';
+import NavBar from "./components/NavBar"
 
 
 function App() {
   return (
     <div>
-      <h1 className="text-4xl font-bold">Hello</h1>
+      <NavBar />
     </div>
   )
 };
