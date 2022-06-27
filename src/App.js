035c040ca8cc348e89ed15ffe38d103a3ef0c915
Deena Smith <deenasmith1630@gmail.com>
2022-06-27 11:27:47 -0500

    linkedin tab added to homepage and styled, other tabs started

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import Home from "./components/Home"
 import NavBar from "./components/NavBar"
+import SocialLinks from './components/SocialLinks';
 
 
 function App() {
@@ -9,6 +10,8 @@ function App() {
     <div>
       <NavBar />
       <Home />
+
+      <SocialLinks />
     </div>
   )
 };
