84f42f06fbe55f34473e08058dc54a9007db1ac5
Deena Smith <deenasmith1630@gmail.com>
2022-06-28 15:43:46 -0500

    adjusted images and styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <div name="about" className="w-fill h-screen bg-gradient-to-b from-gray-800 to-black text-white">
             <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-                <div className="pb-8">
+                <div className="pb-4">
                     <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
                 </div>
                     <p className="text-xl mt-20">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae tempora at dignissimos vel id iste debitis facilis aliquam ducimus. Culpa vitae numquam velit iure optio, dicta totam molestiae dolores, voluptate, aut sit. Voluptate, veritatis ducimus doloribus esse laboriosam nulla. Voluptatum maxime recusandae deserunt dignissimos mollitia quidem velit officiis repudiandae consequatur.
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -4,7 +4,7 @@ import employeeTracker from "../assets/projects/employeeTracker.jpeg";
 import passwordGenerator from "../assets/projects/passwordGenerator.jpeg";
 import pizzaHunt from "../assets/projects/pizzaHunt.jpeg";
 import rateMyPlaylist from "../assets/projects/rateMyPlaylist.jpeg";
-import readMeGenerator from "../assets/projects/readMeGenerator.jpeg";
+import workdayScheduler from "../assets/projects/workdayScheduler.jpeg";
 import satelliteTracker from "../assets/projects/satelliteTracker.jpeg";
 
 const Portfolio = () => {
@@ -16,11 +16,11 @@ const Portfolio = () => {
         },
         {
             id: 2,
-            src: readMeGenerator
+            src: employeeTracker
         },
         {
             id: 3,
-            src: employeeTracker
+            src: workdayScheduler
         },
         {
             id: 4,
@@ -28,11 +28,11 @@ const Portfolio = () => {
         },
         {
             id: 5,
-            src: passwordGenerator
+            src: pizzaHunt
         },
         {
             id: 6,
-            src: pizzaHunt
+            src: passwordGenerator
         }
     ];
 
@@ -43,7 +43,7 @@ const Portfolio = () => {
             className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
         >
             <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-                <div className="pb-8">
+                <div className="pb-10">
                     <p className="text-4xl font-bold inline border-b-4 border-gray-500">
                         Projects
                     </p>
@@ -52,14 +52,14 @@ const Portfolio = () => {
 
                 <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
                     {portfolios.map(({ id, src }) => (
-                        <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+                        <div key={id} className="shadow-md shadow-gray-500 rounded-lg">
                             <img
                                 src={src}
-                                alt=""
+                                alt="projects"
                                 className="rounded-md duration-200 hover:scale-105"
                             />
                             <div className="flex items-center justify-center">
-                                <button className="px-6 py-3 m-4 duration-200 hover:scale-105 font-bold">
+                                <button className="px-6 py-2 m-1 duration-200 hover:scale-105 text-2xl">
                                     Code
                                 </button>
                             </div>
