3f08580dd6a74dd14c91a15d3be67d3c385452cc
Deena Smith <deenasmith1630@gmail.com>
2022-06-28 18:14:32 -0500

    language icons added and styled, experience section complete

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -7,21 +7,91 @@ import html from "../assets/icons/html.png";
 import javascript from "../assets/icons/javascript.png";
 import node from "../assets/icons/node.png";
 import tailwind from "../assets/icons/tailwind.png";
+import mongodb from "../assets/icons/mongodb.png";
 
 
 const Experience = () => {
+
+    const icons = [
+        {
+            id: 1,
+            src: react,
+            title: "React",
+            style: "shadow-sky-400"
+        },
+        {
+            id: 2,
+            src: github,
+            title: "GitHub",
+            style: "shadow-white"
+        },
+        {
+            id: 3,
+            src: javascript,
+            title: "JavaScript",
+            style: "shadow-yellow-400"
+        },
+        {
+            id: 4,
+            src: graphql,
+            title: "GraphQL",
+            style: "shadow-pink-400"
+        },
+        {
+            id: 5,
+            src: html,
+            title: "HTML",
+            style: "shadow-orange-500"
+        },
+        {
+            id: 6,
+            src: tailwind,
+            title: "TailwindCSS",
+            style: "shadow-sky-400"
+        },
+        {
+            id: 7,
+            src: node,
+            title: "NodeJS",
+            style: "shadow-green-300"
+        },
+        {
+            id: 8,
+            src: css,
+            title: "HTML",
+            style: "shadow-blue-500"
+        },
+        {
+            id: 9,
+            src: mongodb,
+            title: "MongoDB",
+            style: "shadow-green-600"
+        }
+    ];
+
     return (
-        <div>
-            <div>
+        <div
+            name="experience"
+            className="bg-gradient-to-b from-gray-800 to-black w-full h-screen"
+        >
+            <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
                 <div>
-                    <p>Experience</p>
-                    <p>These are just a few of the technologies I have worked with...</p>
+                    <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
+                        Experience
+                    </p>
+                    <p className="py-6">These are some of the technologies I've worked with...</p>
                 </div>
-                <div>
-                    <div>
-                        <img src={react} alt="React" />
-                        <p>React</p>
-                    </div>
+
+                <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
+                    {icons.map(({ id, src, title, style }) => (
+                        <div
+                            key={id}
+                            className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
+                        >
+                            <img src={src} alt="" className="w-20 mx-auto" />
+                            <p className="mt-4">{title}</p>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
