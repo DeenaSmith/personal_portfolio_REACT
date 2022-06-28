d1ff712c830a7f09350c7ca89fb0cba8b6ad3dee
Deena Smith <deenasmith1630@gmail.com>
2022-06-28 10:41:24 -0500

    project image files added, html started

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react'
+import employeeTracker from "../assets/projects/employeeTracker.jpeg"
+import passwordGenerator from "../assets/projects/passwordGenerator.jpeg"
+import pizzaHunt from "../assets/projects/pizzaHunt.jpeg"
+import rateMyPlaylist from "../assets/projects/rateMyPlaylist.jpeg"
+import readMeGenerator from "../assets/projects/readMeGenerator.jpeg"
+import satelliteTracker from "../assets/projects/satelliteTracker.jpeg"
+
+
+const Portfolio = () => {
+    return (
+        <div></div>
+    )
+};
+
+
+
+export default Portfolio;
