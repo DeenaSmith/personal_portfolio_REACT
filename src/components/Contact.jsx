b7be6f849328ce10470a41bd50044e009e1117e4
Deena Smith <deenasmith1630@gmail.com>
2022-06-29 11:27:05 -0500

    contact form styled and functional via getform

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,14 +3,15 @@ import React from 'react'
 
 const Contact = () => {
     return (
-        <div>
-            <div>
-                <div>
-                    <p>Get in touch!</p>
-                    <p>Complete the form below and I'll get back to you ASAP.</p>
+        <div name="contact" className="w-full f-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white">
+            <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
+                <div className="pb-8">
+                    <p className="text-4xl font-bold inline border-b-4 border-gray-400">Get in touch!</p>
+                    <p className="py-6">Complete the form below and I'll get back to you ASAP.</p>
                 </div>
-                <div>
-                    <form action="">
+                <div className="flex justify-center items-center">
+                    <form action="https://getform.io/f/70c2ecfe-ec06-4088-af89-1df02b9e25ef"
+                            method="POST" className="flex flex-col w-full md:w-1/2">
                         <input 
                             type="text" 
                             name="name" 
@@ -20,9 +21,10 @@ const Contact = () => {
                             type="text" 
                             name="email" 
                             placeholder="Email" 
-                            className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none" />
+                            className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none" />
                         <textarea 
                             name="message"
+                            placeholder="Type message here!"
                             rows="10"
                             className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none" />
                         <button className="text-white bg-gradient-to-b from-purple-900 to-pink-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">Let's chat!</button>
